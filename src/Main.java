dbf54d645d917aa737872a8bf1482b82bb603a6a
Vlad-Bayran <154011337+Vlad-Bayran@users.noreply.github.com>
2024-03-11 23:06:50 +0300

    Надо будет посмотреть, как другие сделали. Какой-то страшный алгоритм

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,34 @@ public class Main {
     public static void main(String[] args) {
 
         Random random = new Random();
-        int col = 20;
-        int arr1 [] = new int[col];
+        int col = 1000;
+        int arr1[] = new int[col];
 
         for (int i = 0; i < col; i++) {
-
             int r = random.nextInt(col);
             System.out.print(" " + r);
-
             arr1[i] = r;
 
 
         }
-            System.out.println();
+        System.out.println();
 
-        for ( int arr : arr1){
-            System.out.print(" " +arr);
+        for (int i = 0; i < col - 1;) {
+            if (arr1[i] <= arr1[i + 1]) {
+                i++;
+            } else {
+                int step = arr1[i];
+                arr1[i] = arr1[i + 1];
+                arr1[i + 1] = step;
+
+                if (i > 0) {
+                    i--;
+                }
+            }
+        }
+
+        for (int arr : arr1) {
+            System.out.print(" " + arr);
         }
 
     }
